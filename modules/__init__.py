c148218de70063f8c6b3112044563db2272dfc82
dimachep1408 <150166590+dimachep1408@users.noreply.github.com>
2024-02-10 08:16:42 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,8 @@
+from .informator_db.informator_db import connection, cursor
+from .informator_db.cleaning_nicknames import clear_list_nicknames
+from .informator_db.cleaning_passwords import clear_list_passwords
+from .bot_modules.bot.bot import bot
+from .bot_modules.dispatcher.dispatcher import dispatcher
+from .__settings__ import users_Nicknames_list, trash_messages_list, chat_list, flag, flag_client, flag_reg
+from .bot_modules.objects.keyboards.keyboards import keyboard, keyboard2, keyboard4
 
